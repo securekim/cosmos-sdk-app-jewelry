6842efa6d907e948e3e37a0c960ed84d39024343
bro.kim <admin@securekim.com>
2019-07-20 11:03:40 +0900

    [Feature] Add sleep for blockchain

--- nscli.sh
+++ nscli.sh
@@ -1,6 +1,9 @@
 #/bin/bash
+code=code12
 bin/nscli query account $(nscli keys show jack -a)
-bin/nscli tx nameservice buy-code code1 5nametoken --from jack
-bin/nscli tx nameservice set-code code1 carat1 cut1 clarity1 color1 fluorescence1 --from jack
-bin/nscli query nameservice resolve code1
-bin/nscli query nameservice whichis code1
+bin/nscli tx nameservice buy-code $code 5nametoken --from jack
+sleep 3
+bin/nscli tx nameservice set-code $code carat1 cut1 clarity1 color1 fluorescence1 --from jack
+#bin/nscli query nameservice resolve $code
+sleep 5
+bin/nscli query nameservice whichis $code
